e002bc50188ec530144f98a87b8cc7d7aa0d01a7
abhisheky1817 <abhisheky.1817@gmail.com>
2025-06-26 17:37:28 +0000

    add info of variable in js

---
+++ basic1/variables.js
@@ -0,0 +1,11 @@
+// variable -> keyword -> let
+let email = 'abhishek@gmail.com';
+
+// constant -> keyword -> const
+const anotherEmail = 'abhishek@gmail.com';
+
+
+const password = "12345";
+
+// password ="123";
+console.log(password);

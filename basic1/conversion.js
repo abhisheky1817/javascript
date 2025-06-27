29374461fda15a6c8740b0606e2a1be2976cbaf6
abhisheky1817 <abhisheky.1817@gmail.com>
2025-06-27 21:54:05 +0000

    add converion in js

---
+++ basic1/conversion.js
@@ -0,0 +1,24 @@
+// typecasting
+
+let bankbalance = '100';
+//paseint is use for conversion string into int we can use "Number" or "parseint"
+let bankbalancenum = Number(bankbalance);
+let bankbalanceint = parseInt(bankbalance);
+
+// output 100 100 but we want to know the type for the use -> "typeof"
+console.log(bankbalance);
+console.log(bankbalanceint);
+
+console.log(typeof bankbalance);
+console.log(typeof bankbalanceint);
+
+
+let convboolean = Boolean(bankbalance);
+console.log(bankbalance);
+
+
+
+
+
+
+
